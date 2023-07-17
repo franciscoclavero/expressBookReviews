4b759fc0eab2d8f39e56f8fba9358736283134ea
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-07-16 21:48:18 -0300

    feat: commit data

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,37 +6,67 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      if (!isValid(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+    const author = req.params.author;
+    let bookswithsameauthor = [];
+
+    for (let i = 1; i < 10; i++) {
+        console.log('entrou');
+
+        if (books[i].author.indexOf(author) != -1) {
+            bookswithsameauthor.push(books[i]);
+        }
+    }
+    res.send(bookswithsameauthor);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
+    const title = req.params.title;
+    let bookswithsameauthor = [];
+
+    for (let i = 1; i < 10; i++) {
+        console.log('entrou');
+
+        if (books[i].title.indexOf(title) != -1) {
+            bookswithsameauthor.push(books[i]);
+        }
+    }
+    res.send(bookswithsameauthor);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
+    const isbn = req.params.isbn;
+    res.send(books[isbn].title);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
